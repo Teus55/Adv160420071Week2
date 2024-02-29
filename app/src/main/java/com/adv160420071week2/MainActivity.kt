9af6864f60014b7e0fe4fd7955e5c05fd5cd74e5
Teus55 <allanmateus55@gmail.com>
2024-02-29 13:25:29 +0700

    init

---
+++ app/src/main/java/com/adv160420071week2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adv160420071week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
